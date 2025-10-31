1f7a036d39257c30035cfd00487bc2516e1161ce
ChefMilo <collinteo2003@gmail.com>
2025-10-31 14:27:20 +0000

    help

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,47 +1,62 @@
 <script setup>
-    // Import BlogPost component
-    import blogPost from './subcomponents/BlogPost2.vue'
-	import axios from 'axios'
+// Import BlogPost component
+import blogPost from './subcomponents/BlogPost2.vue'
+import axios from 'axios'
 </script>
 
 <script>
-    export default {
-        data() {
-            return {
-                posts: [] // array of post objects
-            }  
-        },
-        computed: {
-            baseUrl() {
-                if (window.location.hostname == 'localhost')
-                    return 'http://localhost:3000'
-                else {
-                    const codespace_host = window.location.hostname.replace('5173', '3000')
-                    return `https://${codespace_host}`;
-                }
-            }
-        },
-        created() { // created is a hook that executes as soon as Vue instance is created
-            axios.get(`${this.baseUrl}/posts`)
-            .then(response => {
-                // this gets the data, which is an array
-                this.posts = response.data
-                console.log(response.data)
-            })
-            .catch(error => {
-                this.posts = [{ entry: 'There was an error: ' + error.message }]
-            })
-        },
-        methods: {
-            deletePost(id) {
-                // TODO: Complete the delete method
-            }
-        }
+export default {
+  data() {
+    return {
+      posts: [] 
+    }  
+  },
+  computed: {
+    baseUrl() {
+      if (window.location.hostname == 'localhost') {
+        return 'http://localhost:3000'
+      } else {
+        const codespace_host = window.location.hostname.replace('5173', '3000')
+        return `https://${codespace_host}`
+      }
     }
+  },
+  created() { 
+    axios.get(`${this.baseUrl}/posts`)
+      .then(response => {
+        this.posts = response.data
+        console.log(response.data)
+      })
+      .catch(error => {
+        this.posts = [{ id: -1, subject: 'Error', entry: 'There was an error: ' + error.message, mood: 'neutral' }]
+      })
+  },
+  methods: {
+    async deletePost(id) {
+      try {
+        await axios.get(`${this.baseUrl}/deletePost`, { params: { id } })
+        this.posts = this.posts.filter(p => p.id !== id)
+      } catch (err) {
+        alert('Failed to delete post: ' + err.message)
+      }
+    }
+  }
+}
 </script>
 
 <template>
-   <!-- TODO: make use of the 'blog-post' component to display the blog posts -->
-
+  <div class="container my-4">
+    <div class="row g-3">
+      <div class="col-12 col-md-6 col-lg-4" v-for="p in posts" :key="p.id">
+        <blogPost :subject="p.subject" :entry="p.entry" :mood="p.mood">
+          <template #actions>
+            <button class="btn btn-outline-danger btn-sm"
+                    @click="deletePost(p.id)">
+              Delete
+            </button>
+          </template>
+        </blogPost>
+      </div>
+    </div>
+  </div>
 </template>
-
--- src/components/subcomponents/BlogPost2.vue
+++ src/components/subcomponents/BlogPost2.vue
@@ -1,13 +1,32 @@
 <script>
-    export default {
-        // Add code if/as necessary
-        // Hint: Add properties subject, entry and mood
+export default {
+  props: {
+    subject: { type: String, required: true },
+    entry:   { type: String, required: true },
+    mood:    { type: String, default: 'neutral' } // e.g., 'happy', 'sad', 'neutral'
+  },
+  computed: {
+    moodImg() {
+      const map = {
+        happy: '/assets/happy.png',
+        sad: '/assets/sad.png',
+        neutral: '/assets/neutral.png'
+      }
+      return map[this.mood?.toLowerCase()] ?? map.neutral
     }
+  }
+}
 </script>
 
-
 <template>
-    <!-- TODO: add your template code here Use boostrap card --> 
-
+  <div class="card h-100 shadow-sm">
+    <img :src="moodImg" class="card-img-top" alt="mood image" style="object-fit: contain; max-height: 180px;">
+    <div class="card-body">
+      <h5 class="card-title mb-2">{{ subject }}</h5>
+      <p class="card-text">{{ entry }}</p>
+    </div>
+    <div class="card-footer bg-transparent d-flex justify-content-end gap-2">
+      <slot name="actions"></slot>
+    </div>
+  </div>
 </template>
-
